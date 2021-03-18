a3e5860ce1f9a6c4127b2ba5331e0d4bf713fe23
Aviral Gangwar <aviralgangwar24@gmail.com>
2021-03-18 20:20:15 +0530

    fix directory struct and port leftover files

--- ui-panel-hello-react/src/App.jsx
+++ ui-panel-hello-react/src/App.jsx
@@ -1,77 +1,53 @@
-const React = require('react');
+import React, { useState } from 'react';
 
-const styles = require('./App.css');
+import './App.css';
 
-const Hello = require('./components/Hello');
-const Clock = require('./components/Clock');
-const ColorPicker = require('./components/ColorPicker');
+import Hello from './components/Hello';
+import Clock from './components/Clock';
+import ColorPicker from './components/ColorPicker';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
+export const App = (props) => {
+    [Color, setColor] = useState({r: 255, g: 0, b: 0, a: 1});
 
-        this.state = {
-            color: {
-                r: 255,
-                g: 0,
-                b: 0,
-                a: 1,
-            },
-        };
-
-        this.panel = React.createRef();
-        this.documentStateChanged = this.documentStateChanged.bind(this);
-        this.colorChanged = this.colorChanged.bind(this);
-    }
-
-    documentStateChanged(selection) {
+    const documentStateChanged = (selection) => {
         if (selection.items.length > 0) {
-            const { Color } = require('scenegraph');
+            import { Color } from 'scenegraph';
             const color = selection.items[0].fill;
             if (color instanceof Color) {
-                this.setState({
-                    color: {
-                        r: color.r,
+                setColor({
+                        r: color.r, 
                         g: color.g,
                         b: color.b,
-                        a: color.a / 255,
-                    },
-                });
+                        a: color.a / 255
+                    });
             }
         }
     }
 
-    colorChanged(color) {
-        this.setState({
-            color: {
-                r: color.r,
-                g: color.g,
-                b: color.b,
-                a: color.a
-            },
-        }, () => {
-            const { r, g, b, a } = this.state.color;
-            const { editDocument } = require("application");
-            const { selection, Color } = require("scenegraph");
-            editDocument({editLabel: "Change Colors"}, () => selection.items.forEach(item => item.fill = new Color(`rgba(${r}, ${g}, ${b}, ${a})`)));
-        }
-        );
+    const colorChanged = (color) => {
+        // this.setState({
+        //     color: {
+        //         r: color.r,
+        //         g: color.g,
+        //         b: color.b,
+        //         a: color.a
+        //     },
+        // }, () => {
+        //     const { r, g, b, a } = this.state.color;
+        //     const { editDocument } = require("application");
+        //     const { selection, Color } = require("scenegraph");
+        //     editDocument({editLabel: "Change Colors"}, () => selection.items.forEach(item => item.fill = new Color(`rgba(${r}, ${g}, ${b}, ${a})`)));
+        // }
+        // );
     }
 
-    render() {
-        const { selection } = this.props;
-        const {
-            color: { r, g, b, a },
-        } = this.state;
-
-        return (
-            <panel className={styles.panel}>
-                <Hello text="Panels!" />
-                <Clock />
-                <ColorPicker r={r} g={g} b={b} a={a} onChange={this.colorChanged} />
-            </panel>
-        );
-    }
+    const { selection } = props;
+    const { r, g, b, a } = Color;
+    return (
+        <panel className="panel">
+            <Hello text="Panels!" />
+            <Clock />
+            <ColorPicker r={r} g={g} b={b} a={a} onChange={colorChanged} />
+        </panel>
+    );
 }
-
-module.exports = App;
--- ui-panel-hello-react/src/main.jsx
+++ ui-panel-hello-react/src/main.jsx
@@ -1,9 +1,6 @@
 // shims, in case they aren't present in the current environment
 require("./util/reactShim");
 
-const React = require("react");
-const ReactDOM = require("react-dom");
-
 const App = require("./App");
 const PanelController = require("./controllers/PanelController");
 
