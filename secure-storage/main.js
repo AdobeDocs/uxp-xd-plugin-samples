9f6885ae02cb1d8a162a591dd81b06938e47d2cc
Ayan Choudhary <ayanchoudhary1025@gmail.com>
2021-03-25 23:07:13 +0530

    update manifest from v3 to v4

--- secure-storage/main.js
+++ secure-storage/main.js
@@ -1,66 +1,19 @@
 const secureStorage = require("uxp").storage.secureStorage;
-let panel;
 
-function create() {
-    const HTML = `
-        <style>
-            .icon {
-                width: 24px;
-                height: 24px;
-                overflow: hidden;
-            }
-            #display {
-                margin-top: 16px;
-                margin-bottom: -16px;
-            }
-        </style>
-        <form method="dialog">
-            <sp-textfield  id="securekey">
-                <sp-label slot="label">Enter your secure key</sp-label>
-            </sp-textfield>
-            <sp-body id="display">
-            </sp-body>
-            <footer>
-                <button id="fetch">Fetch Key</button>
-                <button type="submit" id="save" uxp-variant="cta">Save Key</button>
-            </footer>
-        </form>`;
-
-    async function saveSecureKey() {
-        const userInput = panel.querySelector("#securekey").value;
-        await secureStorage.setItem("secure", userInput);
-        panel.querySelector("#securekey").value = "";
-    }
-
-    async function fetchSecureKey() {
-        // We get the stored value from the secureStorage in the form of a uint8Array.
-        const uintArray = await secureStorage.getItem("secure");
-        // We convert the uint8Array to a string to present it to the user.
-        let secureKey = Array.from(uintArray, i => String.fromCharCode(i)).join("");
-        panel.querySelector("#display").textContent = `
-        Secure Key entered is ${secureKey}`;
-    }
-
-    panel = document.createElement("div");
-    panel.innerHTML = HTML;
-    panel.querySelector("#save").addEventListener("click", saveSecureKey);
-    panel.querySelector("#fetch").addEventListener("click", fetchSecureKey);
-
-    return panel;
+async function saveSecureKey() {
+  const userInput = document.getElementById("securekey").value;
+  await secureStorage.setItem("secure", userInput);
+  document.getElementById("securekey").value = "";
 }
 
-function show(event) {
-// create panel the first time it's shown
-    if (!panel) {
-        panel = create();
-        event.node.appendChild(panel);
-    }
+async function fetchSecureKey() {
+  // We get the stored value from the secureStorage in the form of a uint8Array.
+  const uintArray = await secureStorage.getItem("secure");
+  // We convert the uint8Array to a string to present it to the user.
+  let secureKey = Array.from(uintArray, i => String.fromCharCode(i)).join("");
+  document.getElementById("display").textContent = `
+  Secure Key entered is ${secureKey}`;
 }
 
-module.exports = {
-    panels: {
-        "secure-storage": {
-            show
-        }
-    }
-};
+document.getElementById("save").addEventListener("click", saveSecureKey);
+document.getElementById("fetch").addEventListener("click", fetchSecureKey);
