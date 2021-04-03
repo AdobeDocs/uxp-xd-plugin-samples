a85fb182201917b1cc98b5da0e45f005b147fba1
Aviral Gangwar <aviralgangwar24@gmail.com>
2021-04-03 12:21:43 +0530

    add hello form component

---
+++ ui-panel-hello-react/src/components/HelloForm.jsx
@@ -0,0 +1,32 @@
+import reactShim from "../util/reactShim";
+import styles from "./HelloForm.css";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const HelloForm = (props) => {
+    const [Name, setName] = useState(props.name || "");
+
+    const onInputChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const onDoneClick = (e) => {
+        props.dialog.close();
+    }
+
+    return (
+        <form style={{ width: 300 }}>
+            <h1>React with JSX Components</h1>
+                <label> 
+                    <span>What is your name?</span>
+                    <input onChange={onInputChange} />
+                </label>
+                <p className={styles.text}>{"Hello " + Name}</p>
+                <footer>
+                    <button type="submit" uxp-variant="cta" onClick={onDoneClick}>Done</button>
+                </footer>
+        </form>   
+    )
+}
+
+export default HelloForm;
